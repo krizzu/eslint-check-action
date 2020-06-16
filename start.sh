baf3e3f37c0cd9fa40ffbc1c7035899776588859
Robbie Trencheny <me@robbiet.us>
2020-06-16 16:26:06 -0700

    Yarn or npm

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ cd $GITHUB_WORKSPACE
 
 if [ ! -d "node_modules" ]; then
     echo "Repo dependencies not installed. Installing..."
-    yarn install
+    [ -f yarn.lock ] && yarn install
+    [ -f package-lock.json ] && npm install
 fi
 
 cd "/eslint_check_action" || exit
